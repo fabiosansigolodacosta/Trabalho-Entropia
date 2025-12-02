9035a58c8bd975421c284402e20b04196a09417f
fabiosansigolodacosta <fabiosansigolodacosta@gmail.com>
2025-12-02 11:27:18 -0300

    Comentários adicionais
    Alguns comentários para entender melhor o código

--- trabalho_entropia_final_animacao.py
+++ trabalho_entropia_final_animacao.py
@@ -53,19 +53,19 @@ def draw_grid(start_x, start_y, rows, cols, color):
     pen.penup()
     pen.goto(start_x, start_y)
     pen.pendown()
-    for _ in range(2):
+    for _ in range(2):                    # Desenha o quadrado maior externo
         pen.forward(cols * CELL_SIZE)
         pen.left(90)
         pen.forward(rows * CELL_SIZE)
         pen.left(90)
     pen.width(1)
     pen.color("lightgray")
-    for i in range(1, cols):
+    for i in range(1, cols):              # Desenha as colunas dos quadrados menores dentro do grid
         pen.penup()
         pen.goto(start_x + i * CELL_SIZE, start_y)
         pen.pendown()
         pen.goto(start_x + i * CELL_SIZE, start_y + rows * CELL_SIZE)
-    for i in range(1, rows):
+    for i in range(1, rows):             # Desenha as linhas dos quadrados menores dentro do grid
         pen.penup()
         pen.goto(start_x, start_y + i * CELL_SIZE)
         pen.pendown()
@@ -77,15 +77,15 @@ def render_atoms(atom_dict, base_x, base_y, color):
     diametro = CELL_SIZE * 0.8
     for pos in atom_dict.values():
         x_fisico, y_fisico = pos
-        screen_x = base_x + (x_fisico * CELL_SIZE) + (CELL_SIZE / 2)
-        screen_y = base_y + (y_fisico * CELL_SIZE) + (CELL_SIZE / 2)
+        screen_x = base_x + (x_fisico * CELL_SIZE) + (CELL_SIZE / 2)     # A pos. x na tela da animação. No centro do quadrado
+        screen_y = base_y + (y_fisico * CELL_SIZE) + (CELL_SIZE / 2)     # Mesma coisa p/ o y
         atom.goto(screen_x, screen_y)
-        atom.dot(diametro)
+        atom.dot(diametro)                 # Desenha o círculo com diametro valendo 80% do tamanho do quadrado
 
 # --- LAYOUT (Ambiente Esq | Tanque Dir) ---
 BASE_X_AMBIENTE = -400
 BASE_Y_AMBIENTE = -100
-BASE_X_TANQUE = BASE_X_AMBIENTE + (A_LARG * CELL_SIZE) + GRID_SPACING
+BASE_X_TANQUE = BASE_X_AMBIENTE + (A_LARG * CELL_SIZE) + GRID_SPACING    # A posição do tanque na tela depende da posição do ambiente
 BASE_Y_TANQUE = -100
 
 draw_grid(BASE_X_AMBIENTE, BASE_Y_AMBIENTE, A_ALT, A_LARG, "black")
@@ -109,7 +109,6 @@ for j in range(0, T_ALT):
         atomos_tanque[i] = [k,j]
         i += 1
 
-# MUDANÇA: Não fazemos nada aqui. O tanque começa cheio.
 
 grid_tanque = np.zeros((T_ALT, T_LARG), dtype=int)
 grid_ambiente = np.zeros((A_ALT, A_LARG), dtype=int)
@@ -125,23 +124,23 @@ QUADRADO_AMBIENTE = [A_LARG - 1, 0]
 
 print("Iniciando animação...")
 
-# --- LOOP PRINCIPAL ---
+# --- LOOP PRINCIPAL --- 
 for frame in range(movs_restantes):
     
     # 1. Renderiza
-    atom.clear()
-    hud.clear()
-    render_atoms(atomos_ambiente, BASE_X_AMBIENTE, BASE_Y_AMBIENTE, "royalblue")
-    render_atoms(atomos_tanque, BASE_X_TANQUE, BASE_Y_TANQUE, "darkorange")
+    atom.clear()      # Primeiro limpa para garantir que nenhum átomo vai ficar por cima do outro
+    hud.clear()       # Mesma coisa
+    render_atoms(atomos_ambiente, BASE_X_AMBIENTE, BASE_Y_AMBIENTE, "royalblue")    # Atualiza a nova posição de cada átomo no tanque
+    render_atoms(atomos_tanque, BASE_X_TANQUE, BASE_Y_TANQUE, "darkorange")         # Atualiza o ambiente
     
-    # MUDANÇA: O 'frame' agora começa do 0 real (não +2) pois não teve movimento prévio
-    hud.write(f"Movimento: {frame + 1}\nCarro na posição: {posicao_carro}", 
+    
+    hud.write(f"Movimento: {frame + 1}\nCarro na posição: {posicao_carro}",    # frame começa do 0, por isso somamos 1
               align="center", font=("Arial", 16, "bold"))
     
-    wn.update()
+    wn.update()              # Como o tracer foi definido igual a 0 no início do código 
+                             # temos que atualizar a janela pra aparecer tudo de uma vez
     time.sleep(VELOCIDADE)
-    
-    # 2. Lógica
+    # 2. Mesma lógica do outro código
     todos_atomos_ids = list(atomos_tanque.keys()) + list(atomos_ambiente.keys())
     atom_id_selecionado = random.choice(todos_atomos_ids)
 
@@ -171,11 +170,11 @@ for frame in range(movs_restantes):
                 grid_ambiente[target_y, target_x] = 1
         else:
             if (0 <= nova_pos[0] < T_LARG) and (0 <= nova_pos[1] < T_ALT):
-                mtx_y = T_ALT - 1 - nova_pos[1]
-                mtx_x = nova_pos[0]
-                if grid_tanque[mtx_y, mtx_x] == 0:
+                mat_y = T_ALT - 1 - nova_pos[1]
+                mat_x = nova_pos[0]
+                if grid_tanque[mat_y, mat_x] == 0:
                     grid_tanque[T_ALT - 1 - pos_atual[1], pos_atual[0]] = 0
-                    grid_tanque[mtx_y, mtx_x] = 1
+                    grid_tanque[mat_y, mat_x] = 1
                     atomos_tanque[atom_id_selecionado] = nova_pos
 
     elif espaco_atual == 'ambiente':
@@ -190,11 +189,11 @@ for frame in range(movs_restantes):
                 grid_tanque[target_y, target_x] = 1
         else:
             if (0 <= nova_pos[0] < A_LARG) and (0 <= nova_pos[1] < A_ALT):
-                mtx_y = A_ALT - 1 - nova_pos[1]
-                mtx_x = nova_pos[0]
-                if grid_ambiente[mtx_y, mtx_x] == 0:
+                mat_y = A_ALT - 1 - nova_pos[1]
+                mat_x = nova_pos[0]
+                if grid_ambiente[mat_y, mat_x] == 0:
                     grid_ambiente[A_ALT - 1 - pos_atual[1], pos_atual[0]] = 0
-                    grid_ambiente[mtx_y, mtx_x] = 1
+                    grid_ambiente[mat_y, mat_x] = 1
                     atomos_ambiente[atom_id_selecionado] = nova_pos
 
 print("Fim da simulação.")
